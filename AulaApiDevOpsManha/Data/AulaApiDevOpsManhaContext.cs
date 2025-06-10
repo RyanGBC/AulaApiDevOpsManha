43951151d3c65298b81a5bbd20286411db7f59ee
Aluno <Aluno@UPGT-SPBR-PC148>
2025-06-10 10:37:24 -0300

    Adicionar arquivos de projeto.

---
+++ AulaApiDevOpsManha/Data/AulaApiDevOpsManhaContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using AulaApiDevOpsManha.Models;
+
+namespace AulaApiDevOpsManha.Data
+{
+    public class AulaApiDevOpsManhaContext : DbContext
+    {
+        public AulaApiDevOpsManhaContext (DbContextOptions<AulaApiDevOpsManhaContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<AulaApiDevOpsManha.Models.Produtos> Produtos { get; set; } = default!;
+    }
+}
---
+++ AulaApiDevOpsManha/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using AulaApiDevOpsManha.Data;
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<AulaApiDevOpsManhaContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("AulaApiDevOpsManhaContext") ?? throw new InvalidOperationException("Connection string 'AulaApiDevOpsManhaContext' not found.")));
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
